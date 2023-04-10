480cb20667b4aef59b75e62bab1905e820e028b1
Mamat <reut.itzhaki@campus.technion.ac.il>
2023-04-10 22:05:28 +0300

    started hw1

---
+++ HW1/scrape_news.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+site="https://www.ynetnews.com/category/3082"
+data=$(wget --no-check-certificate -O - $site)
+
+articles=$(echo "$data" | grep -oP "https://www.ynetnews.com/article/[0-9a-zA-Z]+" | sort | uniq )
+echo "$articles" | wc -l > result.csv 
+
+output=""
+num=("" "" "" "")
+(( counter=0 ))
+for art in $articles; do
+	info=$(wget --no-check-certificate -O - $art 2>/dev/null)
+	for name in "Netanyahu" "Gantz" "Bennett" "Peretz"; do
+		num[$counter]=$(echo "$info" | grep -oP $name | wc -w )
+        (( counter++ ))	
+	done
+	if [[ "$num" -eq "0000" ]]; then
+	  output="$art, -"
+	else
+      output="$art, Netanyahu, ${num[0]}, Gantz, ${num[1]}, Bennett, ${num[2]}, Peretz, ${num[3]}"
+	fi
+
+	echo "$output" >> result.csv
+	(( counter=0 ))
+	num=("" "" "" "")  
+done
